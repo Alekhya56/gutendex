b7c753ed0c3d9141ac1f93a7838ba8e0bc6d8ede
Alekhya56 <51811563+Alekhya56@users.noreply.github.com>
2025-05-26 19:11:57 +0530

    Updated allowed_hosts

--- gutendex/settings.py
+++ gutendex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ul2dyaha-06bfks9*a^o-=coqo19tkc!_mnxd!6477s&ar=uph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['gutendex-voci.onrender.com',]
 
 
 # Application definition
